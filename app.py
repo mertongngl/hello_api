ead44ce94378ecd4ed06c8f7f5eb4f7459431328
Mert Öngengil <mertongngl@gmail.com>
2019-12-05 05:46:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ class Bcfm(Resource):
 api.add_resource(Bcfm, '/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5353, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
